b92e85f7362ad6dbba7ed6105aeedf8d973190e8
Santiago <santiidele21@gmail.com>
2024-07-11 21:23:19 -0300

    first commit

---
+++ Backend/api_pampa22/src/main/java/com/api/pampa22/api_pampa22/ApiPampa22Application.java
@@ -0,0 +1,13 @@
+package com.api.pampa22.api_pampa22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPampa22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPampa22Application.class, args);
+	}
+
+}
